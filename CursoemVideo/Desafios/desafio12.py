302ac7e528a3e80fa0180db6441a8f99dacc01d8
William Ricky Soares <wrcsoares@gmail.com>
2022-01-13 11:03:03 -0400

    Atividades realizadas em 12/01/2022

--- CursoemVideo/Desafios/desafio12.py
+++ CursoemVideo/Desafios/desafio12.py
@@ -1,6 +1,11 @@
 """
 # Desafio 012
-
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
-
 """
+
+print('# Desafio 012 '
+      '\n# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, '
+      'com 5% de desconto.')
+
+p = float(input('Qual o valor do produto? >>> R$'))
+print('O valor do produto com desconto de 5%, ou seja R${:.2f}, fica no total R${:.2f} '.format(p * 5/100, p - p * 5/100))
--- CursoemVideo/Desafios/desafio13.py
+++ CursoemVideo/Desafios/desafio13.py
@@ -2,3 +2,10 @@
 # Desafio 013
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.
 '''
+
+print('# Desafio 013 '
+      '\n# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.')
+
+s = float(input('Qual o valor do salário? >>> R$'))
+print('O valor do salário é de {:.2f}, adicionando 15%, ou seja R${:.2f}, totaliza R${:.2f}'
+      .format(s, s * 15/100, s + s * 15/100))
