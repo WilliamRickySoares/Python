8510d5e44d010df6144111c44752ad928f7c1ff4
William Ricky Soares <wrcsoares@gmail.com>
2022-01-25 11:15:31 -0400

    Atualizações de 25/01/22

--- CursoemVideo/Desafios/desafio021.py
+++ CursoemVideo/Desafios/desafio021.py
@@ -1,8 +1,9 @@
 import pygame
 
-
-print('\nFaça um programa em Python que abra e reproduza um audio de um arquivo MP3')
-pygame.init()
-pygame.mixer.music.load('hebrides.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+while True:
+    print('\nFaça um programa em Python que abra e reproduza um audio de um arquivo MP3')
+    pygame.init()
+    pygame.mixer.music.load('moolight.mp3')
+    pygame.mixer.music.play()
+    pygame.event.wait()
+# TODO: Verificar forma de parar o script depois da execução do arquivo de áudio
