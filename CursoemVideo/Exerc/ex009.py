52553b15eec4fe0c2de70af08d95c58002e06507
William Ricky Soares <wrcsoares@gmail.com>
2022-02-04 08:53:58 -0400

    Atualizações de 04/02/2022

---
+++ CursoemVideo/Exerc/ex009.py
@@ -0,0 +1,24 @@
+frase = 'Curso em Video Python'
+print(frase)
+print('\nA frase um total de {} caracteres.'.format(len(frase)))
+print('A letra 3 da frase = "{}".'.format(frase[3]))
+print('O trecho do 9 ao 14 = "{}".'.format(frase[9:14]))
+print('O trecho a partir do 15 em diante = "{}".'.format(frase[15:]))
+print('O trecho até o até o 14 = "{}".'.format(frase[:14]))
+print('Pegar um trecho e pular de 2 em 2 = "{}"'.format(frase[1:15:2]))
+print('Pegar toda a frase pular de 3 em 3 = "{}"'.format(frase[::3]))
+
+print('\nBloco de textos\n')
+print('''"Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit, sed do eiusmod t
+empor incididunt ut labore et dolore magna 
+aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco laboris 
+nisi ut aliquip ex ea commodo consequat. 
+Duis aute irure dolor in reprehenderit in 
+voluptate velit esse cillum dolore eu fugiat 
+nulla pariatur. Excepteur sint occaecat cupidatat 
+non proident, sunt in culpa qui officia deserunt 
+mollit anim id est laborum."''')
+
+
