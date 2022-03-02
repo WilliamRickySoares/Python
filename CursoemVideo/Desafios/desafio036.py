b7397261c0992f37e99aec662cf3a135650b8e41
William Ricky Soares <wrcsoares@gmail.com>
2022-03-01 20:43:19 -0400

    Atualizações 01/03/2022

--- CursoemVideo/Desafios/desafio036.py
+++ CursoemVideo/Desafios/desafio036.py
@@ -5,3 +5,17 @@ O programa vai perguntar o valor da casa, o salário do comprador e em quantos a
 Calcule o valor da prestação de mensal, sabendo que ela não pode esceder 30% do salário
 ou então o empréstimo será negado
 """
+
+valor_casa = float(input('Informe o valor do imóvel: '))
+limite = 0.3 * float(input('Informe o valor do salário: '))
+anos = int(input('Quantos anos para efetuar o pagamento: '))
+parcela = valor_casa / (anos * 12)
+if parcela > limite:
+    print(f'\nA solicitação de emprestimo de R$ {valor_casa:.2f} foi \033[1:31mNEGADO\033[m.'
+          f'\nO solicitante tem o limite de R$ {limite:.2f} '
+          f'e o valor da parcela é de R$ {parcela:.2f}!')
+else:
+    print(f'\nA solicitação de emprestimo de R$ {valor_casa:.2f} foi \033[1:32mAPROVADO\033[m.'
+          f'\nO solicitante tem o limite de R$ {limite:.2f} '
+          f'e o valor da parcela é de R$ {parcela:.2f} por mês durante {anos} ano(s).')
+
