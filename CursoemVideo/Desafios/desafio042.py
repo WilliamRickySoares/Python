8ba33874e9cf4020de5d0d733dc5ad9e9962b72f
William Ricky Soares <wrcsoares@gmail.com>
2022-04-06 14:16:16 -0400

    Atualizações 06/04/2022

--- CursoemVideo/Desafios/desafio042.py
+++ CursoemVideo/Desafios/desafio042.py
@@ -2,7 +2,8 @@
 Desafio 042
 Refaça o Desafio 035 dos triangulos,
 acrescentando o recurso de mostrar que tipo de triângulo será formado:
-- Equilátero: todos os lados iguais
-- Isósceles: dois lados iguais
-- Escaleno: todos os lados diferentes
+    - Equilátero: todos os lados iguais
+    - Isósceles: dois lados iguais
+    - Escaleno: todos os lados diferentes
 """
+
--- CursoemVideo/Desafios/desafio043.py
+++ CursoemVideo/Desafios/desafio043.py
@@ -4,7 +4,27 @@ Desenvolva uma lógica que leia o peso e a altura de uma pessoa,
 calcule seu IMC e mostre seu status, de acordo com a tabela:
     - Abaixo de 18.5: Abaixo do peso
     - Entre 18.5 e 25: Peso ideal
-    - 25 até 30: sobrepeso
+    - 25 até 30: Sobrepeso
     - 30 até 40: Obsesidade
     - Acima de 40: Obseidade mórbida
 """
+
+print('\n\033[1mCLACULADO DE IMC - ÍNDICIE DE MASSA CORPORAL\033[m')
+
+peso = float(input('\nDigite o seu peso: '))
+altura = float(input('Digite a sua altura: '))
+imc = peso / altura
+status = str('status')
+
+if imc <= 18.5:
+    status = str('Abaixo do peso')
+elif imc < 25:
+    status = str('Peso ideal')
+elif imc < 30:
+    status = str('Sobrepeso')
+elif imc < 40:
+    status = str('Obseidade')
+elif imc > 40:
+    status = str('Obseidade mórbida')
+
+print(f'\nO seu imc é de {imc:.2f} com o status \033[1m{status}\033[m')
