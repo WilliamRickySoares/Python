69eb86ba305b78d6e23c1e6c22d6b4a2a59fda54
William Ricky Soares <wrcsoares@gmail.com>
2022-04-22 16:40:17 -0400

    Atualizações 22/04/2022

--- CursoemVideo/Desafios/desafio044.py
+++ CursoemVideo/Desafios/desafio044.py
@@ -7,3 +7,22 @@ considerando o seu preço e a condição de pagamento:
     - Em até 2x no cartão: preço normal
     - 3x ou mais no cartão: 20% de juros
 """
+
+preco = float(input('Digite o valor do produto: '))
+print('\n[ 1 ] À vista dinheiro / cheque: 10% de desconto')
+print('[ 2 ] À vista no cartão: 5% de desconto')
+print('[ 3 ] Em até 2x no cartão: preço normal')
+print('[ 4 ] 3x ou mais no cartão: 20% de juros')
+pgto = int(input('Escolha uma opção de pagamento: '))
+
+
+if pgto == 1:
+    pago = preco - preco * 0.10
+elif pgto == 2:
+    pago = preco - preco * 0.05
+elif pgto == 3:
+    pago = preco * 1
+elif pgto == 4:
+    pago = preco + preco * 0.20
+
+print('\nO valor pago será de R$ {:.2f}!'.format(pago))
