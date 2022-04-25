273cd908f4a7e59c1547283fcd041dacf5fc6188
William Ricky Soares <wrcsoares@gmail.com>
2022-04-25 10:01:35 -0400

    Atualizações 25/04/2022

--- CursoemVideo/Desafios/desafio044.py
+++ CursoemVideo/Desafios/desafio044.py
@@ -9,20 +9,28 @@ considerando o seu preço e a condição de pagamento:
 """
 
 preco = float(input('Digite o valor do produto: '))
-print('\n[ 1 ] À vista dinheiro / cheque: 10% de desconto')
+print('\n\033[1mAs opções de pagamento são:\033[m')
+print('[ 1 ] À vista dinheiro / cheque: 10% de desconto')
 print('[ 2 ] À vista no cartão: 5% de desconto')
 print('[ 3 ] Em até 2x no cartão: preço normal')
 print('[ 4 ] 3x ou mais no cartão: 20% de juros')
-pgto = int(input('Escolha uma opção de pagamento: '))
-
+pgto = int(input('Escolha uma das opções de pagamento: '))
+forma = 'uma opção inválida'
+pago = 0
 
 if pgto == 1:
+    forma = 'à vista no dinheiro / cheque com 10% de desconto'
     pago = preco - preco * 0.10
 elif pgto == 2:
+    forma = 'à vista no cartão com 5% de desconto'
     pago = preco - preco * 0.05
 elif pgto == 3:
+    forma = 'parcelado em 2x no cartão sem juros'
     pago = preco * 1
 elif pgto == 4:
+    forma = 'parcelado em 3x ou mais no cartão com 20% de juros'
     pago = preco + preco * 0.20
+else:
+    pgto = ''
 
-print('\nO valor pago será de R$ {:.2f}!'.format(pago))
+print('\nA forma de pagamento escolhida foi de {} e o valor pago deverá ser de R$ {:.2f}!'.format(forma, pago))
