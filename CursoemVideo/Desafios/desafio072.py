f91aa931f472781b8cb7cf8a3d21c6eb79f80009
William Ricky Soares <wrcsoares@gmail.com>
2022-08-07 18:49:05 -0400

    Exercícios 07/08/2022

--- CursoemVideo/Desafios/desafio072.py
+++ CursoemVideo/Desafios/desafio072.py
@@ -1,17 +1,28 @@
-print("""Desafio 072
+print("""Desafio 072 (M3A16)
 Crie um programa que tenha uma tupla totalmente preenchida com uma contagem
 por extenso, de zero até vinte.
 
 Seu programa deverá ler um numero pelo teclado (entre 0 e 20) e mostrá-lo
-por extenso.""")
+por extenso.\n""")
 
-tupla = ('um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze'
-         , 'quatorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
+tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+         'seis', 'sete', 'oito', 'nove', 'dez',
+         'onze', 'doze', 'treze', 'quatorze', 'quinze',
+         'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
 
 while True:
-    num = int(input('\nDigite um número entre 1 e 20: '))
-    if 1 <= num <= 20:
-        print(f'Você digitou {tupla[num - 1]}.')
+    num = int(input('Digite um número entre 1 e 20: '))
+    if 0 <= num <= 20:
+        print(f'Você digitou {tupla[num]}.')
     else:
-        print(f'Foi digitado um valor fora do intervalo solicitado. O programa será encerrado!')
+        print(f'\nVocê digitou {num}, favor digitar um número entre 1 e 20. ', end='')
+
+    e = ' '
+    while e not in 'SsNn':
+        e = str(input('\nDeseja continuar? [S/N] ')).strip().upper()[0]
+    if e in 'Nn':
         break
+
+# q = ''
+# while q not in 'NnSs':
+
