b3a12e94e4d97611910aaf9d7c8ce906ae4e1065
William Ricky Soares <wrcsoares@gmail.com>
2022-08-07 22:16:23 -0400

    Exercícios 07/08/2022

--- CursoemVideo/Aula/ex009.py
+++ CursoemVideo/Aula/ex009.py
@@ -16,6 +16,19 @@ print('Localizar a posição do caracter = {}'.format(frase.find('o')))
 print('Substituir na visualização "Python" por "Android" = "{}"'.format(frase.replace('Python', 'Android')))
 print('O trecho "Curso" esta na frase? {}'.format('Curso' in frase))
 
+print('\n****************************\n')
+nome = 'José'
+idade = 33
+salario = 987.35
+print(f'O {nome} tem {idade} anos e ganha R${salario:.2f}')
+print(f'Esse nome {nome:20} é lindo') #20 espaços
+print(f'Esse nome {nome:^20} é lindo') #20 espaços centralizado
+print(f'Esse nome {nome:-^20} é lindo') #20 espaços centralizado com traço
+print(f'Esse nome {nome:-<20} é lindo') #alinhado a esquerda
+print(f'Esse nome {nome:->20} é lindo') #alinhado a direita
+
+print('\n****************************\n')
+
 print('\nBloco de textos\n')
 print('''"Lorem ipsum dolor sit amet, 
 consectetur adipiscing elit, sed do eiusmod t
@@ -33,3 +46,39 @@ print('Procurar depois de colocar tudo em minusculo = {}'.format(frase.lower().f
 print('Frase separada por cada espaço "{}"'.format(frase.split()))
 print('Depois de fazer o split da frase, ela se torna uma lista. '
       'Podemos atribuir o resultado e chamar somente um dos itens da lista')
+
+print("""
+Transformação
+frase.replace(‘Python’, ‘Android’) » vai substituir Python por Android
+
+frase.upper() » coloca toda a frase em maiúsculo.
+
+frase.lower() » coloca toda a frase em minúsculo.
+
+frase.capitalize() » deixa somente a primeira letra da string em maiúsculo.
+
+frase.title() » deixa a primeira letra de todas as palavras em maiúsculo.
+
+frase.strip() » elimina todos os espaços no início e no fim.
+
+frase.rstrip() » elimina todos os espaços da direita.
+
+frase.lstrip() » elimina todos os espaços da esquerda.
+
+Divisão
+frase.split() » cria uma divisão onde na string onde tem espaço. E cada separação é recomeçada a contagem. Cria uma lista.
+
+Junção
+‘-‘.join(frase) » junta os elemento de frase usando como separador o – .
+
+Usando dois comandos
+frase.lower().find(‘video’)
+
+Tirar os espaços de uma string
+c = frase.split()
+frasejunta = ”.join(c)
+
+Inverso de uma string
+frase[::-1]
+
+""")
