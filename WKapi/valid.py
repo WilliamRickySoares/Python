3949d4724749180f030c97f12b2529e52c9bb0f3
William Ricky Soares <89048945+WilliamRickySoares@users.noreply.github.com>
2023-10-20 21:39:29 -0400

    Delete WKapi directory

--- WKapi/valid.py
+++
@@ -1,6 +0,0 @@
-import zeep
-
-wsdl = 'http://webservices.wk.com.br/Areas/Empresarial/Empresarial.svc?singleWsdl'
-client = zeep.Client(wsdl=wsdl)
-print(client.service.Method1('Zeep', 'is cool'))
-print(wsdl)
