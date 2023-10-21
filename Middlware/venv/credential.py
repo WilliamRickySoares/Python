89b687fda37224e661e9c5cde3f3cde36de358b3
William Ricky Soares <wrcsoares@gmail.com>
2023-10-20 21:45:01 -0400

    Update .gitignore

---
+++ Middlware/venv/credential.py
@@ -0,0 +1,8 @@
+import socket
+host = socket.gethostname()
+
+base_webservice = 'TOTAL'
+user_webservice = 'FUSION'
+senha_webservice = 'GsiTNjR*@LuoL3'
+server_base = host
+data_base = 'middleware'
