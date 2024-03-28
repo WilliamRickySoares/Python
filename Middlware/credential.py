7e102f66071e723240859cae2505014b61abd732
William Ricky Soares <wrcsoares@gmail.com>
2024-03-27 22:06:07 -0400

    Ocultar senhas

--- Middlware/credential.py
+++
@@ -1,8 +0,0 @@
-import socket
-host = socket.gethostname()
-
-# base_webservice = 'TOTAL'
-# user_webservice = 'FUSION'
-# senha_webservice = 'GsiTNjR*@LuoL3'
-server_base = host
-data_base = 'middleware'
